e57e5dd47ed501f438860c436208f9b8626a3ccb
Santhosh <santhoshkumar872000@gmail.com>
2026-01-01 02:13:48 +0530

    feat: mobile create password ui

--- lib/presentation/pages/forgot_password/widgets/mobile_create_password.dart
+++ lib/presentation/pages/forgot_password/widgets/mobile_create_password.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:instagram_clone/core/constants/app_colors.dart';
+import 'package:instagram_clone/core/constants/app_constants.dart';
+import 'package:instagram_clone/core/constants/app_styles.dart';
+import 'package:instagram_clone/presentation/widgets/app_button.dart';
+import 'package:instagram_clone/presentation/widgets/app_text_field.dart';
 
 class MobileCreatePassword extends StatelessWidget {
   final RxBool smsLink;
@@ -12,6 +18,117 @@ class MobileCreatePassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+        final RxBool check = false.obs;
+    return SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Obx(()=>Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    IconButton(onPressed: (){
+                    
+                    }, icon: Icon(Icons.arrow_back_ios,color: kWhite,)),
+                    SizedBox(
+                      height: kHeight*2,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text("Create a new password",
+                      style: kAppTextTheme.bodyLarge?.copyWith(
+                        fontSize: 20.h
+                      ),),
+                    ),
+                     SizedBox(
+                      height: kHeight*2,
+                    ),
+                     Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Create a password with at least 6 letters and numbers. You'll need this password to log in to your account.",style: kAppTextTheme.bodyLarge?.copyWith(
+                                fontSize: 12.h,
+                                
+                              ),
+                            ),
+                            
+                          ]
+                        ),
+                      ),
+                    ),
+                     SizedBox(
+                      height: kHeight*2,
+                    ),
+                    AppTextField(
+                              label: "New password",
+                              textEditingController: TextEditingController(),
+                              hintText:"New password",
+                          
+                            ),
+                      
+                            SizedBox(
+                              height: kHeight*2,
+                            ),
+                            AppButton(
+                              label: "Continue",
+                              onTap: (){
+                              
+                              },
+                            ),
+                            
+                            SizedBox(
+                              height: kHeight*2,
+                            ),
+                            
+                            AppButton(
+                          label: "Skip",
+                          onTap: (){
+                            
+                          },
+                          outline: true,
+                        ),
+                         Row(
+                              children: [
+                                Checkbox(value: check.value, onChanged: (val){
+                                  check.value=!check.value;
+                                },
+                                side: BorderSide(
+                                  color: kDisabledButtonBackground
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4.0),
+                                  
+                                ),
+                                activeColor: kSelectedColor,),
+                                Expanded(
+                                  child: RichText(
+                                    text: TextSpan(
+                                      children: [
+                                        TextSpan(
+                                          text:"Log out of other devices. Choose this if someone else used your account.",style: kAppTextTheme.bodyLarge?.copyWith(
+                                            fontSize: 10.h,
+                                            color: kDisabledButtonBackground
+                                            
+                                          ),
+                                        ),
+                                       
+                                      ]
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                  ],
+                ),
+              ),),
+            ),
+             
+          ],
+        ),
+      );
   }
 }
